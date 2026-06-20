1fc7170932ad2a0f8e1f2d60ff7c4863459843c7
teguh-dayanto-online <teguh.dayanto.online@gmail.com>
2026-06-20 20:44:02 +0700

    The web app is ready

--- app.py
+++ app.py
@@ -30,7 +30,6 @@ def predict_api():
     print(output[0])
     return jsonify(output[0])
 
-'''
 @app.route('/predict',methods=['POST'])
 def predict():
     data=[float(x) for x in request.form.values()]
@@ -38,8 +37,6 @@ def predict():
     print(final_input)
     output=regmodel.predict(final_input)[0]
     return render_template("home.html",prediction_text="The House price prediction is {}".format(output))
-'''
-
 
 if __name__=="__main__":
     app.run(debug=True)
